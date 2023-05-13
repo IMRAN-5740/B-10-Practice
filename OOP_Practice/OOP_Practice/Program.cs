04d27b7fc84f78c9611e809da977f8746234d629
imran <imranbsmrstucse@gmail.com>
2023-05-13 23:15:29 +0600

    Object Describe

--- OOP_Practice/OOP_Practice/Program.cs
+++ OOP_Practice/OOP_Practice/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using OOP_Practice.SalesModule;
+
+Console.WriteLine("Welcome to Sales Screen");
+Console.WriteLine("Please Provide a Sales Record....");
+
+Sales salesObj = new Sales();
+
+
+Console.WriteLine("Provide Sales num :....");
+int salesNo = Convert.ToInt32(Console.ReadLine());
+salesObj.SalesNum = salesNo;
+
+Console.WriteLine("Provide Customer Name :....");
+string  customerName =Console.ReadLine();
+salesObj.CustomerName = customerName;
+
+Console.WriteLine("Provide Customer Contact :....");
+string customerContact =Console.ReadLine();
+salesObj.CustomerContact = customerContact;
+string dateTime=DateTime.Now.ToString("dd/MMM/yyyy/hh/mm/ss/tt");
+
+salesObj.SalesDate = dateTime;
+Console.WriteLine("Provide Product name :....");
+string productName = Console.ReadLine();
+salesObj.ProductName = productName;
+
+Console.WriteLine("Provide Quantity Of a Product :....");
+int quantity = Convert.ToInt32(Console.ReadLine());
+salesObj.Quantity = quantity;
+
+Console.WriteLine("Provide UnitPrice Of a Product :....");
+decimal unitPrice = Convert.ToDecimal(Console.ReadLine());
+salesObj.UnitPrice = unitPrice;
+
+
+
+
+
+Console.WriteLine("Your Purchase Invoice is Show Below: .........");
+Console.WriteLine(salesObj.GetInvoice());
---
+++ OOP_Practice/OOP_Practice/SalesModule/Sales.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice.SalesModule
+{
+    public class Sales
+    {
+        public int SalesNum { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerContact { get; set; }
+        public string SalesDate { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public string GetInvoice()
+        {
+            string inVoice = "";
+            inVoice += $"Sales Num :{SalesNum}\n";
+            inVoice += $"CustomerName  :{CustomerName}\n";
+            inVoice += $"Customer Contact  :{CustomerContact}\n";
+            inVoice += $"ProductName  :{ProductName}\n";
+            inVoice += $"Product Quantity  :{Quantity}\n";
+            inVoice += $"UnitPrice  :{UnitPrice}\n";
+            inVoice += $"TotalPrice  :{Quantity*UnitPrice}\n";
+
+            return inVoice;
+        }
+
+    }
+   
+}
